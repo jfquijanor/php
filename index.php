6f50674596996e3891b781f59748d7f4eb70e466
jfquijanor <jfquijanor@unadvirtual.edu.co>
2015-10-16 17:41:42 -0500

    Namespaces y preparación de proyecto ejemplo

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
 <?php
+
+	spl_autoload_register(function($clase){
+		//print $clase;
+		$ruta = "api/" . str_replace("\\", "/", $clase) . ".php";
+		//print $ruta;
+		if (is_readable($ruta)) {
+			require_once $ruta;
+		}else{
+			echo "El archivo no existe";
+		}
+	});
+
+	/*
+	require_once "api/Models/Persona.php";
+	require_once "api/Controllers/PersonasController.php";  
+	*/
+	//Persona::Hola(); //Class 'Persona' not found, debe incluirse previamente el namespace
+	
+	Models\Persona::Hola();
+	//Controllers\Persona::Hola();
+	//La clase debe tener el mismo nombre de la carpeta al crear una autoload
+	Controllers\PersonasControllers::Hola();
