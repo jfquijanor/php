5e51d6e21ed681ae095cc4fe30a58138e8d072e8
jfquijanor <jfquijanor@unadvirtual.edu.co>
2015-10-23 16:47:59 -0500

    Configuración de Autoload

--- proyecto/Models/Estudiante.php
+++ proyecto/Models/Estudiante.php
@@ -11,9 +11,16 @@
 		private $fecha;
 		private $con;
 		
-		public function __construct(argument)
-		{
+		public function __construct(){
 			$this->con = new Conexion();
+		}
+
+		public function set($atributo, $contenido){
+			$this->$atributo = $contenido;
+		}
+
+		public function get($atributo){
+			return $this->$atributo;
 		}
 
 		public function listar(){
@@ -42,9 +49,11 @@
 
 		public function view(){
 			$sql = "SELECT t1.*, t2.nombre as nombre_seccion FROM estudiantes t1 INNER JOIN secciones t2 
-			ON t1.id_seccion = t2.id WHERE id = '{$this->id}'";
+			ON t1.id_seccion = t2.id WHERE t1.id = '{$this->id}'";
 			$datos = $this->con->consultaRetorno($sql);
-			return $datos;
+			//return $datos;
+			$row = mysqli_fetch_assoc($datos);
+			return $row;
 		}
 
 	}
